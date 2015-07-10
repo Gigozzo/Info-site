c014f7254f2cb2abfaa6692d3ebcb497af340644
Gigozzo <Gigozzo4@gmail.com>
2015-07-10 16:33:53 +0300

    Page routing. (#9)

--- site_layout/urls.py
+++ site_layout/urls.py
@@ -10,5 +10,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', TemplateView.as_view(template_name='main.html')),
+    url(r'^contact/', TemplateView.as_view(template_name='contact.html')),
+    url(r'^news/', TemplateView.as_view(template_name='news.html')),
+    url(r'^services/', TemplateView.as_view(template_name='services.html')),
+
     url(r'^admin/', include(admin.site.urls)),
 )
