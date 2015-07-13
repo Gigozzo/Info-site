c78182d2383a8e3b204f91e5cf7d761a39b5c1e2
Gigozzo <Gigozzo4@gmail.com>
2015-07-13 10:29:10 +0300

    Add news table. (#12)

--- main_theme/models.py
+++ main_theme/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+	title = models.CharField(max_length=255)
+	content = models.TextField(max_length=10000)
+	datetime = models.DateTimeField(u'Date')
